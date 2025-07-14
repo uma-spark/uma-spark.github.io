c31cc23197280da75569df544bd0577316f5b641
rockisch <rockischplay@gmail.com>
2025-07-14 02:42:49 -0300

    Fix for mobile

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -96,7 +96,6 @@ function CardsPicker({
 	onFullscreen,
 	onDownload,
 	onCopy,
-	notification,
 	hideMenu,
 	rarityFilters,
 	onRarityFilter,
@@ -114,7 +113,7 @@ function CardsPicker({
 		));
 
 	return photoMode ? (
-		<div className="card-picker">
+		<div className="card-picker hidden">
 			<div
 				className={`card-picker-photo-actions ${hideMenu ? "hide-menu" : ""}`}
 			>
@@ -127,12 +126,9 @@ function CardsPicker({
 				{/* biome-ignore format: svg */}
 				<svg onClick={onFullscreen} fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"></path></svg>
 			</div>
-			<span className={`notification ${notification ? "visible" : ""}`}>
-				{notification}
-			</span>
 		</div>
 	) : (
-		<div className="card-picker">
+		<div className="card-picker active">
 			<div className="card-picker-actions">
 				{/* biome-ignore format: svg */}
 				<svg onClick={onClear} fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" ><path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path> 				</svg>
@@ -200,7 +196,6 @@ function App({ cards, bannerIds }) {
 		[rarityFilters, sortedCards],
 	);
 	const [pickerHideMenu, setPickerHideMenu] = useState(false);
-	const [pickerNotification, setPickerNotification] = useState("");
 
 	function selectCard(card) {
 		const _card = { ...card, uid };
@@ -260,11 +255,9 @@ function App({ cards, bannerIds }) {
 			const canvas = await html2canvas(sparkContainer.current);
 			setPickerHideMenu(false);
 			canvas.toBlob(async (blob) => {
-				await navigator.clipboard.write([
+				await navigator.clipboard?.write([
 					new ClipboardItem({ "image/png": blob }),
 				]);
-				setPickerNotification("Copied!");
-				setTimeout(() => setPickerNotification(""), 2000);
 			});
 		}, 0);
 	}
@@ -312,7 +305,6 @@ function App({ cards, bannerIds }) {
 				onFullscreen={onFullscreen}
 				onDownload={onDownload}
 				onCopy={onCopy}
-				notification={pickerNotification}
 				hideMenu={pickerHideMenu}
 				rarityFilters={rarityFilters}
 				onRarityFilter={onRarityFilter}
